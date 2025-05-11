90ff6d7ce7f87bec6321b12c0c7c620e702bbb87
Joshua Cueva <josucueva02@gmail.com>
2025-05-10 19:15:42 -0500

    Springboot server created

---
+++ src/main/java/distributed/systems/sd_cli_java/SdCliJavaApplication.java
@@ -0,0 +1,13 @@
+package distributed.systems.sd_cli_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdCliJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdCliJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/distributed/systems/sd_cli_java/SdCliJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package distributed.systems.sd_cli_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdCliJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

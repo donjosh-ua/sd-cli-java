e974b7c6a1dad18aa8ef1059ff844a4569cc7e14
Joshua Cueva <joshua.cueva@ucuenca.edu.ec>
2025-07-18 08:54:14 -0500

    Commented out amqp dependency

--- src/main/java/distributed/systems/sd_cli_java/SdCliJavaApplication.java
+++ src/main/java/distributed/systems/sd_cli_java/SdCliJavaApplication.java
@@ -1,15 +1,7 @@
 package distributed.systems.sd_cli_java;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 

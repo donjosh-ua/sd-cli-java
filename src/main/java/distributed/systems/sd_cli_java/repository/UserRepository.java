f0936a43a94ff08d5d28b397c10c7fd5abc85578
Joshua Cueva <joshua.cueva@ucuenca.edu.ec>
2025-07-18 21:10:21 -0500

    Updated user repository methods

--- src/main/java/distributed/systems/sd_cli_java/repository/UserRepository.java
+++ src/main/java/distributed/systems/sd_cli_java/repository/UserRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import distributed.systems.sd_cli_java.model.entity.User;
@@ -12,11 +14,11 @@ import distributed.systems.sd_cli_java.model.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
-    // New API methods using email as primary key
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
-    Page<User> findByNicknameContainingIgnoreCase(String trim, Pageable pageable);
+    @Query("SELECT u FROM User u WHERE LOWER(u.nickname) LIKE LOWER(CONCAT('%', :query, '%')) ORDER BY u.nickname ASC")
+    Page<User> findByNicknameContainingIgnoreCase(@Param("query") String query, Pageable pageable);
 
 }
